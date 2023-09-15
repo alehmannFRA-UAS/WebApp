111cb514a7658ebbd4b7d9dbcc7bbfb3a0c3c46d
Kaddour Alnaasan <30522984+kalnaasan@users.noreply.github.com>
2023-09-15 11:26:36 +0000

    add components to declarations

--- ChatApp/src/app/main/main.module.ts
+++ ChatApp/src/app/main/main.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
+import { RoomComponent } from './components/room/room.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RoomComponent,
+    ChatComponent
+  ],
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    FormsModule
   ]
 })
 export class MainModule { }
