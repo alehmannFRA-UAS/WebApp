6491af63b44a2e0a0e78f7eae3eb1b801ca8adf4
Kaddour Alnaasan <30522984+kalnaasan@users.noreply.github.com>
2023-09-15 11:26:07 +0000

    create Route fro component

--- ChatApp/src/app/main/main-routing.module.ts
+++ ChatApp/src/app/main/main-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RoomComponent } from './components/room/room.component';
+import { ChatComponent } from './components/chat/chat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'rooms', pathMatch: 'full' },
+  { path: 'rooms', component: RoomComponent, pathMatch: 'full' },
+  { path: 'rooms/:id', component: ChatComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
