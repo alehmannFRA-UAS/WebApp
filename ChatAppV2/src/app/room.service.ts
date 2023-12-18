5a95201619c9e9027ba38c73f7e41f493d8a70a9
Kaddour Alnaasan <30522984+kalnaasan@users.noreply.github.com>
2023-12-18 10:25:36 +0000

    create RoomService

---
+++ ChatAppV2/src/app/room.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Room } from './room';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoomService {
+
+  constructor(private http: HttpClient) { }
+
+  public getAll(): Observable<Room> {
+    const url = 'http://127.0.0.1:8000/users/1/chatrooms';
+
+    const headers = new HttpHeaders().set('Accept', 'application/json');
+
+    return this.http.get<any>(url, { headers });
+  }
+
+  public createRoom(name: string): Observable<Room> {
+    const url = 'http://127.0.0.1:8000/users/1/chatrooms';
+
+    const headers = new HttpHeaders()
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json');
+
+    return this.http.post<Room>(url, name, { headers });
+  }
+
+  public getRoomByIdandUserId(roomId: string, userId: number): Observable<Room> {
+    const url = `http://127.0.0.1:8000/users/${userId}/chatrooms/${roomId}`;
+
+    const headers = new HttpHeaders().set('Accept', 'application/json');
+
+    return this.http.get<any>(url, { headers });
+  }
+}
