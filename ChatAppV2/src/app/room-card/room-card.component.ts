d09101d21e511d7b81ac89ec0ba979c44d3de03f
Kaddour Alnaasan <30522984+kalnaasan@users.noreply.github.com>
2024-01-22 08:39:12 +0000

    add join-room

--- ChatAppV2/src/app/room-card/room-card.component.ts
+++ ChatAppV2/src/app/room-card/room-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Room } from '../room';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RoomService } from '../room.service';
 
 @Component({
   selector: 'app-room-card',
@@ -11,12 +12,31 @@ export class RoomCardComponent {
 
   @Input() item: Room | null = null;
   @Input() selected = false;
+  private userID = 1;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private roomService: RoomService) {
+    this.activatedRoute.queryParams.subscribe((params: any) => {
+      if (params['userId']) {
+        this.userID = params['userId'];
+        console.log('User-ID: ', this.userID);
 
-  public editRoom(): void {
+      }
+    });
+  }
+
+  public join() {
     if (this.item !== null) {
-      this.router.navigate(['/rooms/', this.item.id]);
+      this.roomService.joinRoom(this.item.id, this.userID).subscribe({
+        next: (res: any) => {
+          console.log(res);
+          if (this.item) {
+            this.router.navigate(['/rooms/', this.item.id, 'conversation'], { queryParams: { userId: this.userID } });
+          }
+        },
+        error: (err: any) => console.log(err)
+      });
     }
   }
 }
